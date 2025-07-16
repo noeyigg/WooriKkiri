0f4cc9a756ba8fb3ece51625afa5f952b6bec505
SooY2 <ssy67366736@gmail.com>
2025-07-16 11:55:35 +0900

    lottie-test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 function App() {
-  return <></>;
+  return (
+    <DotLottieReact
+      src="https://lottie.host/29373c04-9a05-4a12-8b2d-86cda0c082aa/x0ci3AbDhW.lottie"
+      autoplay
+    />
+  );
 }
 
 export default App;
